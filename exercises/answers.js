3f47280061faeed907240470ef6b692b69d6ea5a
EC2 Default User <ec2-user@ip-172-31-19-228.us-east-2.compute.internal>
2019-10-22 18:33:12 +0000

    started

--- exercises/answers.js
+++ exercises/answers.js
@@ -1,27 +1,60 @@
 // Question 1
-function greet() {
-
+function greet(name) {
+  console.log("Good morning, " + name);
 }
+greet("Carmen");
+greet("Devonte");
+greet("Rueben Ogbonna");
+
 
 // Question 2
-function logOddNumbers() {
-
+function logOddNumbers(num) {
+  for  ( let num = 1; num <= 99; num+= 1){
+    if (num % 2 !== 0){
+      console.log(num);
+    }  
+ }
 }
+let num;
+logOddNumbers(num);
 
 // Question 3
-function area() {
-
+function area (length, width){
+  length;
+  width;
+  let area = length * width;
+  return(area);
 }
+
+area(5, 15);
+area(15, 10);
+area(25.75, 42);
+
 
 // Question 4
-function otherAngle() {
-
+let triangle = function(angle1 , angle2){
+  let degree = angle1 + angle2;
+  let angle3 = 180 - degree;
+  console.log(angle3);
+  return angle3;
+  
 }
+
+triangle(30, 60);
+triangle(60, 60);
+triangle(43, 78);
+triangle(10, 20);
 
 // Question 5
-function diameterFromArea() {
-
+function diameterFromArea(num){
+ let diameter = 2 * Math.sqrt(num / Math.PI);
+  console.log(diameter);
+  return diameter;
+  
 }
+
+diameterFromArea(10);
+diameterFromArea(100);
 
 // Question 6
 function totalPrice() {
