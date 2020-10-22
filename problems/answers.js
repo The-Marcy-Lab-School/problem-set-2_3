59bfe21ecf4b23df7d66f28b5bfd06b8a3bcda4d
Ann Duong <30392423+thuyanduong@users.noreply.github.com>
2020-10-22 09:14:45 -0400

    added IFEE to remove browser error

--- problems/answers.js
+++ problems/answers.js
@@ -59,17 +59,20 @@ function stringy() {
 }
 
 // Do NOT touch or write anything below this line
-module.exports = {
-  greet,
-  logOddNumbers,
-  area,
-  otherAngle,
-  diameterFromArea,
-  totalPrice,
-  shortLongShort,
-  describeAge,
-  isLeapYear,
-  multisum,
-  asciiValue,
-  stringy,
-};
+(function(){
+  module.exports = {
+    greet,
+    logOddNumbers,
+    area,
+    otherAngle,
+    diameterFromArea,
+    totalPrice,
+    shortLongShort,
+    describeAge,
+    isLeapYear,
+    multisum,
+    asciiValue,
+    stringy,
+  };
+})();
+
